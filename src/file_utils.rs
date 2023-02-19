0ca0aacd7048d6b5609f25e0b0d37cfa08520f68
im <ispingzhou@gmail.com>
2023-02-19 14:12:41 +0000

    Datas would be write new file after be processed

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -1,17 +1,29 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 
-pub fn read_and_parse_file(filename: &str) -> Result<Vec<(String, i32)>, std::io::Error> {
-    let file = File::open(filename)?;
+pub fn read_file(input_filename: &str) -> Result<Vec<String>, std::io::Error> {
+    let file = File::open(input_filename)?;
     let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+    Ok(lines)
+}
+
+pub fn parse_lines(
+    lines: Vec<String>,
+    output_filename: &str,
+) -> Result<Vec<(String, i32)>, std::io::Error> {
+    let output_file = File::create(output_filename)?;
+    let mut writer = BufWriter::new(output_file);
     let mut result = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
+    for line in lines {
         let parts: Vec<&str> = line.split(',').collect();
         if parts.len() == 2 {
             let name = parts[0].trim().to_string();
             let value = parts[1].trim().parse::<i32>().unwrap_or(0);
-            result.push((name, value));
+            result.push((name.clone(), value));
+            if value >= 10 {
+                writeln!(writer, "{},{}", name, value)?;
+            }
         }
     }
     Ok(result)
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 mod file_utils;
 
 fn main() -> std::io::Result<()> {
-    let data = file_utils::read_and_parse_file("lines.txt")?;
-    for (name, value) in data {
-        if value >= 10 {
-            println!("{}: {}", name, value);
+    let input_filename = "input.txt";
+    let output_filename = "output.txt";
+    let lines = file_utils::read_file(input_filename)?;
+    let data = file_utils::parse_lines(lines, output_filename);
+    match data {
+        Ok(data) => {
+            println!("Data processed successfully. Results:");
+            for (name, value) in &data {
+                println!("{}: {}", name, value);
+            }
+        }
+        Err(err) => {
+            eprintln!("Error processing data: {}", err);
         }
     }
     Ok(())
