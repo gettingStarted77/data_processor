f926f0d3d30c2705c512af19fd2128f37abc269f
im <ispingzhou@gmail.com>
2023-02-21 14:38:21 +0000

    mainly add regex crate to parse lines of file

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -1,40 +1,51 @@
+use regex::Regex;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::path::Path;
+use std::io::{BufRead, BufReader, BufWriter, Error, ErrorKind, Write};
+use std::path::{Path, PathBuf};
 
-pub fn read_file(input_filename: &str) -> Result<Vec<String>, std::io::Error> {
-    let path = Path::new(input_filename);
-    if !path.exists() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            format!("File not found: {}", path.display()),
-        ));
-    }
-    if !path.is_file() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidInput,
-            format!("{} is not a file.", path.display()),
-        ));
-    }
-
-    let file = File::open(input_filename)?;
+pub fn read_file(input_filename: &str) -> Result<Vec<String>, Error> {
+    let file = match File::open(input_filename) {
+        Ok(file) => file,
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                eprintln!("Input file not found: {}", input_filename);
+            } else {
+                eprintln!("Error opening input file: {}", e);
+            }
+            return Err(e);
+        }
+    };
     let reader = BufReader::new(file);
     let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
     Ok(lines)
 }
 
-pub fn parse_lines(
-    lines: Vec<String>,
-    output_filename: &str,
-) -> Result<Vec<(String, i32)>, std::io::Error> {
-    let output_file = File::create(output_filename)?;
+pub fn parse_line(line: &str) -> Option<(String, i32)> {
+    let re = Regex::new(r#"^\s*(?P<name>\S+)\s*,\s*(?P<value>-?\d+)\s*$"#).unwrap();
+    let caps = re.captures(line)?;
+    let name = caps["name"].to_string();
+    let value = caps["value"].parse().unwrap();
+    Some((name, value))
+}
+
+pub fn parse_lines(lines: Vec<String>, output_filename: &str) -> Result<Vec<(String, i32)>, Error> {
+    let path = Path::new(output_filename);
+    let output_file = if path.exists() && path.is_file() {
+        let new_path = generate_new_filename(&path);
+        println!(
+            "The file {} already exists. Saving to {} instead.",
+            path.display(),
+            new_path.display()
+        );
+        File::create(new_path)?
+    } else {
+        File::create(output_filename)?
+    };
+
     let mut writer = BufWriter::new(output_file);
     let mut result = Vec::new();
     for line in lines {
-        let parts: Vec<&str> = line.split(',').collect();
-        if parts.len() == 2 {
-            let name = parts[0].trim().to_string();
-            let value = parts[1].trim().parse::<i32>().unwrap_or(0);
+        if let Some((name, value)) = parse_line(&line) {
             result.push((name.clone(), value));
             if value >= 10 {
                 writeln!(writer, "{},{}", name, value)?;
@@ -42,4 +53,13 @@ pub fn parse_lines(
         }
     }
     Ok(result)
+}
+
+fn generate_new_filename(path: &Path) -> PathBuf {
+    let new_filename = format!(
+        "{}_{}.txt",
+        path.file_stem().unwrap().to_str().unwrap(),
+        chrono::Utc::now().format("%Y-%m-%d_%H-%M-%S")
+    );
+    path.with_file_name(new_filename)
 }
